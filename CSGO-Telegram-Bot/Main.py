c73c95c26dda5a64e2930e9af34e027d65e3b048
Finn Reinhardt <65501534+FinnJakobR@users.noreply.github.com>
2022-09-17 00:10:57 +0200

    Update Main.py

--- CSGO-Telegram-Bot/Main.py
+++ CSGO-Telegram-Bot/Main.py
@@ -1,9 +1,6 @@
-
-from ast import While
-from email.message import Message
+import json
 from time import sleep, time
-from tkinter.messagebox import RETRY
-from SendMessages import SEND_MESSAGE
+from SendMessages import MESSAGE_LISTENER, SEND_MESSAGE
 from SendMessages import RECIEVE_MESSAGE
 from SendMessages import SEND_CALENDAR
 from counterstrike import counterstrike
@@ -13,17 +10,19 @@ from ics import Calendar, Event
 import threading
 
 BOT_ID = "BOT_ID"
-CHAT_ID = 1239294392 
+CHAT_ID = 2328482
 
 Commands = [];
 
-DAYS = 0
+DAYS = 3
 UPCOMMING_TOURNAMENTS = []
+LAST_TOURNAMENT = None
+FOLLOW_UPCOMMING_MATCHES = []
 
 currentMonth = datetime.datetime.now().month
 currentDay = datetime.datetime.now().day
 
-TODAY =  datetime.date.today().strftime( "%m/%d/%y")
+TODAY =  datetime.date.today().strftime("%m/%d/%y")
 
 
 def GenerateDatePlusDays():
@@ -34,9 +33,70 @@ def GenerateDatePlusDays():
 
     return end_date.strftime("20%y-%m-%d")
 
+def GetUpcommingMatches():
+    csgo_obj = counterstrike("EVENT_REMINDER")
+    followed = GetFollowedEvents()
+    UppcommingMatches = csgo_obj.get_upcoming_and_ongoing_games()
+    for match in UppcommingMatches:
+        if(IsAllreadySaved(match) and not IsFollowed(followed)):
+            FOLLOW_UPCOMMING_MATCHES.remove(match)
+        
+        Date = int(match["start_time"])
+        if(IsStartedToday(Date) and IsFollowed(followed, match["tournament"]) and not IsAllreadySaved(match)):
+            FOLLOW_UPCOMMING_MATCHES.append(match)
+
+    threading.Timer(60.0, GetUpcommingMatches).start()
+    return
+
+def CheckTime():
+
+    for match in FOLLOW_UPCOMMING_MATCHES:
+        Date = int(match["start_time"])  
+        ItsTime(Date)
+    
+    threading.Timer(1.0, CheckTime).start()
+
+    return
+
+
+def ItsTime(unix):
+    Time = datetime.datetime.utcfromtimestamp(unix).strftime("%H:%M:%S")
+    currentTime = datetime.datetime.now().strftime("%H:%M:%S")
+
+    print(Time)
+
+    if(Time == currentTime):
+        SEND_MESSAGE(BOT_ID, CHAT_ID, "TEST")
+    
+    return
+
+
+
+
+
+
+def IsStartedToday(unix):
+    Date = datetime.datetime.utcfromtimestamp(unix).strftime("%m/%d/%y")
+    if(Date == TODAY):
+        return True
+    return False
+
+def IsFollowed(FollowedTournament, Tournament):
+    if(Tournament.lower() in  FollowedTournament):
+        return True
+    
+    return False
+
+def IsAllreadySaved(match):
+    if(match in FOLLOW_UPCOMMING_MATCHES):
+        return True
+    
+    return False
+
+
+
 
 def GetEventsFromLiquidPedia():
-    threading.Timer(86000.0, GetEventsFromLiquidPedia).start()
     currentDay = datetime.datetime.now().day
     currentMonth = datetime.datetime.now().month
     DateTime = GenerateDatePlusDays()
@@ -73,6 +133,7 @@ def GetEventsFromLiquidPedia():
     UPCOMMING_TOURNAMENTS = rightTournaments
 
     ShouldSend(UPCOMMING_TOURNAMENTS)
+    threading.Timer(86000.0, GetEventsFromLiquidPedia).start()
     return
 
 def Getday(date, isOneDay):
@@ -148,7 +209,9 @@ def ShouldSend (tournaments):
             BODY = "*A NEW CSGO EVENT IS COMMING TO TOWN!:*\n🎫: {}\n📍:{}\n🗓️: {}\n🏆: {}\n🌐:[Liquipedia](https://liquipedia.net{}) ".format(tournament["tournament"], tournament["event_locaion"], tournament["date"], tournament["prize"], tournament["link"])
             SEND_MESSAGE(Bot_id=BOT_ID, chat_id=CHAT_ID, body=BODY)
             CreateIcsFile(tournament["tournament"],  tournament["date"])
-            SEND_CALENDAR("./ICS/Data.ics", BOT_ID, CHAT_ID)
+            LAST_TOURNAMENT = tournament["tournament"]
+            SEND_CALENDAR("./ICS/Event.ics", BOT_ID, CHAT_ID)
+            SaveTournament(LAST_TOURNAMENT)
 
 
 def CreateIcsFile(Name, Date):
@@ -158,9 +221,45 @@ def CreateIcsFile(Name, Date):
     e.begin = Date + " 12:30:00"
     c.events.add(e)
     c.events
-    with open("./ICS/Data.ics","w") as my_file:
+    with open("./ICS/Event.ics","w") as my_file:
          my_file.writelines(c.serialize_iter())
 
     return 
 
+def SaveTournament(newTournament):
+    f = open("./Tournaments.json", "r+")
+    if (f != ""):
+         Data = json.load(f)
+         Data.append(newTournament.lower())
+    f.seek(0)
+    f.write(json.dumps(Data))
+    f.truncate()
+    f.close()
+    return
+
+def ReloadTournamentFile():
+    f = open("./Tournaments.json", "r+")
+    if(f == ""):
+        f.close()
+        return
+    
+    f.seek(0)
+    f.write(json.dumps([]))
+    f.truncate()
+    f.close()
+    return
+
+def GetFollowedEvents():
+    f = open("./FollowedTournaments.json")
+    JsonData = json.load(f)
+    f.close()
+    return JsonData
+
+
+    
+GetUpcommingMatches()
+ReloadTournamentFile()
 GetEventsFromLiquidPedia()
+CheckTime()
+MESSAGE_LISTENER(BOT_ID, CHAT_ID)
+
